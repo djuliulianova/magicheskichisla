8e3106c57e153ba1d569fa338fb63ea6b9395762
Student <Student@DESKTOP-JRI0BF7>
2022-11-11 11:27:29 +0200

    Add project files.

---
+++ magicheskichisla/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace magicheskichisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int magicNumber = int.Parse(Console.ReadLine());
+                int proizv = 1;
+
+                for (int i = 100000; i <= 999999; i++)
+                {
+
+                    int n = i;
+
+                    do
+                    {
+                        proizv = proizv * (n % 10);
+                        n = n / 10;
+                    } while (n > 0);
+
+                    if (proizv == magicNumber)
+                    {
+                        Console.Write("{0} ", i);
+                    }
+
+                }
+            }
+        }
+    }
+
+}
+
+
